983dce13309c16a2edc36852e457fb698805e20f
Sifatulla21 <sifat2109@gmail.com>
2022-03-01 12:29:55 +0600

    details part added

--- phone-shop.js
+++ phone-shop.js
@@ -1,10 +1,11 @@
+//search reasult
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    // console.log(searchText);
+    // clear value from input field
     searchField.value = '';
+    // load data
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    // console.log(url);
     fetch(url)
     .then(res => res.json())
     .then(data => displaySearchResult(data.data));
@@ -12,22 +13,28 @@ const searchPhone = () => {
 
 const displaySearchResult = data => {
     const searchResult = document.getElementById('search-result');
-    data.forEach(data => {
-        console.log(data);
-        const div = document.createElement('div');
-        div.classList.add('search');
-        div.innerHTML = `
-          <div class="card h-100 w-50">
-                <img src="${data.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h3 class="card-title">Model: ${data.phone_name}</h3>
-                <h5 class="card-title">Brand: ${data.brand}</h5>
-                <button onclick="loadDetail('${data.slug}')" class="button-details" type="button">Details</button>
-            </div>
-          </div>
-          `;
-          searchResult.appendChild(div);
-    })
+    searchResult.textContent = '';
+    if(data.length == 0){
+        console.log('no result found');
+    }
+    else{
+        data.forEach(data => {
+            const div = document.createElement('div');
+            div.classList.add('search');
+            div.innerHTML = `
+              <div class="card h-100 w-50">
+                    <img src="${data.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h3 class="card-title">Model: ${data.phone_name}</h3>
+                    <h5 class="card-title">Brand: ${data.brand}</h5>
+                    <button onclick="loadDetail('${data.slug}')" class="button-details" type="button">Details</button>
+                </div>
+              </div>
+              `;
+              searchResult.appendChild(div);
+        })
+    }
+
 }
 
 // DETAILS 
@@ -36,11 +43,10 @@ const loadDetail = slug => {
     const url = `  https://openapi.programming-hero.com/api/phone/${slug} `;
     fetch(url)
     .then(res => res.json())
-    .then(data => displayDetails(data));
+    .then(data => displayDetails(data.data));
 }
 
 const displayDetails = data => {
-    console.log(data);
     const detailsResult = document.getElementById('details-result');
         const div = document.createElement('div');
         div.classList.add('details');
@@ -49,7 +55,34 @@ const displayDetails = data => {
                 <img src="${data.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h3 class="card-title">Model: ${data.name}</h3>
-                <h5 class="card-title">Releas Date: ${data.brand} </h5>
+                <h3 class="card-title">${data.releaseDate}</h3>
+                <h3 class="card-title">Main Features:</h3>
+                <ul>
+                    <li>
+                        ${data.mainFeatures.storage}
+                    </li>
+                    <li>
+                        ${data.mainFeatures.displaySize}
+                    </li>
+                    <li>
+                        ${data.mainFeatures.chipSet}
+                    </li>
+                </ul>
+                <h3 class="card-title">Sensors:</h3>
+                <ul>
+                    <li>
+                    ${data.mainFeatures.sensors[0]}
+                    </li>
+                    <li>
+                    ${data.mainFeatures.sensors[1]}
+                    </li>
+                    <li>
+                    ${data.mainFeatures.sensors[2]}
+                    </li>
+                    <li>
+                    ${data.mainFeatures.sensors[3]}
+                    </li>
+                </ul>
             </div>
           </div>
           `;
